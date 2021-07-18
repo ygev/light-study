6f515bdbd1863f5b47e950054ddc540ec2b2d41c
ygev <ygevorgyan@mica.edu>
2021-07-18 15:48:04 -0700

    Initial commit.

---
+++ script.js
@@ -0,0 +1,53 @@
+// Make the DIV element draggable:
+dragElement(document.getElementById("js-light-source"));
+
+function dragElement(elmnt) {
+  var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+  if (document.getElementById(elmnt.id)) {
+    // if present, the header is where you move the DIV from:
+    document.getElementById(elmnt.id).onmousedown = dragMouseDown;
+  } else {
+    // otherwise, move the DIV from anywhere inside the DIV:
+    elmnt.onmousedown = dragMouseDown;
+  }
+
+  function dragMouseDown(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // get the mouse cursor position at startup:
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    document.onmouseup = closeDragElement;
+    // call a function whenever the cursor moves:
+    document.onmousemove = elementDrag;
+  }
+
+  function elementDrag(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // calculate the new cursor position:
+    pos1 = pos3 - e.clientX;
+    pos2 = pos4 - e.clientY;
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    // set the element's new position:
+    elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+
+    console.log("sun position left: " + elmnt.style.left + ", top: " + elmnt.style.top);
+
+    // TODO yana: set #box css to have shadow based off elmnt.style.left, elmnt.style.top
+
+    let x = -20;
+    let y = 20;
+    let blur = 40;
+
+    document.querySelector("#box").style = "box-shadow: -20px 20px 40px rgba(131, 117, 171, 0.5);";
+  }
+
+  function closeDragElement() {
+    // stop moving when mouse button is released:
+    document.onmouseup = null;
+    document.onmousemove = null;
+  }
+}
