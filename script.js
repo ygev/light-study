6ce91a09871d8863425a2cad0a7074b2a7b74641
ygev <ygevorgyan@mica.edu>
2021-07-18 18:42:27 -0700

    MVP sans angle

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
+var box = document.getElementById("box")
 // Make the DIV element draggable:
 dragElement(document.getElementById("js-light-source"));
 
-function dragElement(elmnt) {
+function dragElement(sun) {
   var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
-  if (document.getElementById(elmnt.id)) {
+  if (document.getElementById(sun.id)) {
     // if present, the header is where you move the DIV from:
-    document.getElementById(elmnt.id).onmousedown = dragMouseDown;
+    document.getElementById(sun.id).onmousedown = dragMouseDown;
   } else {
     // otherwise, move the DIV from anywhere inside the DIV:
-    elmnt.onmousedown = dragMouseDown;
+    sun.onmousedown = dragMouseDown;
   }
 
   function dragMouseDown(e) {
@@ -31,18 +32,36 @@ function dragElement(elmnt) {
     pos3 = e.clientX;
     pos4 = e.clientY;
     // set the element's new position:
-    elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
-    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+    sun.style.top = (sun.offsetTop - pos2) + "px";
+    sun.style.left = (sun.offsetLeft - pos1) + "px";
+    // Sun Center
+    var sunTop = (sun.offsetTop - pos2)
+    var sunLeft = (sun.offsetLeft - pos1)
+    var sunCenterX= (sunLeft + (sun.offsetWidth/2))
+    var sunCenterY = (sunTop + (sun.offsetHeight/2)) 
+    // Box Center
+    var boxTop = box.offsetTop;
+    var boxLeft = box.offsetLeft;
+    var boxCenterX= (boxLeft + (box.offsetWidth/2))
+    var boxCenterY = (boxTop + (box.offsetHeight/2)) 
 
-    console.log("sun position left: " + elmnt.style.left + ", top: " + elmnt.style.top);
+    // TODO yana: set #box css to have shadow based off sun.style.left, sun.style.top
 
-    // TODO yana: set #box css to have shadow based off elmnt.style.left, elmnt.style.top
 
-    let x = -20;
-    let y = 20;
-    let blur = 40;
+    var x = -(sunCenterX-boxCenterX)/2;
+    var y = -(sunCenterY-boxCenterY)/2;
+    var blur = (Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2)))*.75;
+    var deg; 
+    var boxShadow = "box-shadow:" +  x + "px " + y + "px " + blur + "px rgba(117, 129, 171, 0.5);";
+    var gradient = "background: linear-gradient(" + deg + "deg, #FFFFFF 50%%, #E4E7F3 100%); ";
+    var newStyles = boxShadow + gradient
 
-    document.querySelector("#box").style = "box-shadow: -20px 20px 40px rgba(131, 117, 171, 0.5);";
+    console.log(blur)
+    // console.log("Sun offsets are " + sun.offsetTop +  " top and " + sun.offsetLeft + " left " + sun.offsetWidth + " width " + sun.offsetHeight + " height ")
+    // console.log("Box offsets are " + box.offsetTop +  " top and " + box.offsetLeft + " left " + box.offsetWidth + " width " + box.offsetHeight + " height ")
+    // console.log("Sun center is (" + sunCenterX + ", " + sunCenterY + ")" );
+    // console.log("Box center is (" + boxCenterX + ", " + boxCenterY + ")" );
+    document.querySelector("#box").style = newStyles;
   }
 
   function closeDragElement() {
